d3e0e8d5c406cc721b75b3f4bfac944073182aa4
kosuke55 <kosuke.tnp@gmail.com>
2024-03-16 23:25:31 +0900

    select port
    Signed-off-by: kosuke55 <kosuke.tnp@gmail.com>

--- daily_scenario_test/plotly_dash_daily_test.py
+++ daily_scenario_test/plotly_dash_daily_test.py
@@ -1,3 +1,5 @@
+import os
+
 import dash
 from dash import dcc, html, dash_table
 import plotly.graph_objs as go
@@ -209,4 +211,5 @@ app.layout = html.Div(
 )
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    port = int(os.environ.get("PORT", 8050))
+    app.run_server(debug=True, host='0.0.0.0', port=port)
